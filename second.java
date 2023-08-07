f0ad5a94db732ed5f1576f9c00bed13c393b36ed
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-07 21:48:10 +0530

    Java second program

---
+++ second.java
@@ -0,0 +1,13 @@
+// To check the size of a datatype and how size it uses.
+
+package packages;
+
+public class second {
+
+	public static void main(String[] args) {
+		
+        System.out.println("Int min no: " + Integer.MIN_VALUE);
+        System.out.println("Int max no: " + Integer.MAX_VALUE);
+	    System.out.println("Int size no: " + Integer.BYTES);
+	}
+}
