62879be2ffc7a1c29dcb63fb90a97f1c9e3e213f
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-08 21:49:27 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,20 @@
+package packages;
+
+public class operators {
+
+	public static void main(String[] args) {
+		
+		int a=25;
+		int b=12;
+		int c=a%b;
+		int d=a/b;
+		int e=a+b;
+		int f=a-b;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+
+	}
+
+}
