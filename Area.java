015fb2cc8b893a612f6931fc129af39705fa3d1e
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-09 18:50:23 +0530

    Area

---
+++ Area.java
@@ -0,0 +1,16 @@
+//Calculate the area of rectangle............... 
+
+
+package packages;
+
+public class Area {
+
+	public static void main(String[] args) {
+		int length=10;
+		int breadth=40;
+		int area = length*breadth;
+		System.out.println("Area of rectangle:\n"+ area);
+
+	}
+
+}
