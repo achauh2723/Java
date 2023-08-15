9b7dcb4fc4f26d4eb51bf317b6d23fb37837b4da
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-15 22:35:26 +0530

    String Input

---
+++ third.java
@@ -0,0 +1,17 @@
+//Taking input from the user in the form of string and printing it
+
+package packages;
+import java.util.*;
+
+public class third {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name:");
+		String str = sc.nextLine();
+		System.out.println("String is:\n"+str);
+		
+
+	}
+
+}
