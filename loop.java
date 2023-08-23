e3a59d7c93eed593dcfe8671a54f8da658f5fcb9
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-23 22:34:44 +0530

    Series

---
+++ loop.java
@@ -0,0 +1,18 @@
+// Printing series using loop.
+
+package packages;
+
+import java.util.Scanner;
+
+public class loop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number 10");
+		int i = sc.nextInt(); 
+		for(i=0;i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
