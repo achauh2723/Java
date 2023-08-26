4b75089c983c2e2e6b578c343f812d9119c3a41d
achauh2723 <romi.chauhan2275@gmail.com>
2023-08-26 23:14:44 +0530

    multiple objects

---
+++ multipleobjects.java
@@ -0,0 +1,19 @@
+package packages;
+
+public class multipleobjects {
+
+	public static void main(String[] args) {
+		public class multipleobjects {
+			  int x = 5;
+			  public static void main(String[] args) {
+			    multipleobjects myObj1 = new multipleobjects();
+			    multipleobjects myObj2 = new multipleobjects();
+			    System.out.println(myObj1.x);
+			    System.out.println(myObj2.x);
+			  }
+			}
+
+
+	}
+
+}
