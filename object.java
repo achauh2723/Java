92e243543b019c2eecebfbf18c35f8f47f9dfaa2
achauh2723 <romi.chauhan2275@gmail.com>
2023-09-07 20:10:47 +0530

    object

---
+++ object.java
@@ -0,0 +1,18 @@
+package packages;
+
+import java.util.*;
+import java.lang.*;
+
+public class object {
+	void display(){
+		int x = 5;
+		System.out.println(x);
+	}
+
+	public static void main(String[] args) {
+		object obj = new object();
+		System.out.println(obj);
+
+	}
+
+}
