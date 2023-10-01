8dae3dce9c2f59ea998e4f1a77d585e1f093e444
achauh2723 <romi.chauhan2275@gmail.com>
2023-10-01 23:25:27 +0530

    array sum

---
+++ Arraysum.java
@@ -0,0 +1,33 @@
+//code to print the sum of all the elements of array
+
+package packages;
+import java.util.Scanner;
+
+public class Arraysum {
+
+	public static void main(String[] args) {
+		int arr[]= {2,4,6,1};
+		int sum = 0;
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+		}
+		System.out.println("the sum of the elements:"+sum);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
